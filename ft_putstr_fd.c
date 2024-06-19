45aecdc06f4fd5df3375b4172e2a203732a1d33d
Mitsutaka Miura <mmiura@c1r1s4.42tokyo.jp>
2024-06-19 13:57:36 +0900

    Passed tests with ft_putstr_fd and prep for comments

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mmiura <mmiura@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 13:07:34 by mmiura            #+#    #+#             */
-/*   Updated: 2024/06/19 13:52:39 by mmiura           ###   ########.fr       */
+/*   Updated: 2024/06/19 13:56:42 by mmiura           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 #include <unistd.h>
 
 void	ft_putstr_fd(char *s, int fd)
+/**
+ * @brief
+ * @param (s)
+ * @param (fd)
+ * @return (void)
+ * @detail 'can't write more than INT_MAX digits
+ */
 {
 	size_t	i;
 

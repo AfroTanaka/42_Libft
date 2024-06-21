6f31ec13ccbe59eb8569bbb63dda22bafa8fb471
Mitsutaka Miura <mmiura@c1r1s4.42tokyo.jp>
2024-06-21 11:30:06 +0900

    Add comments to ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,13 +6,19 @@
 /*   By: mmiura <mmiura@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 11:15:04 by mmiura            #+#    #+#             */
-/*   Updated: 2024/06/21 11:16:09 by mmiura           ###   ########.fr       */
+/*   Updated: 2024/06/21 11:29:35 by mmiura           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_putendl_fd(char *s, int fd)
+/**
+ * @brief Outputs the string `s` to the given file descriptor followed by a newline.
+ * @param (s) The string to output
+ * @param (fd) The file descriptor on which to write
+ * @return (void) None
+ */
 {
 	if (!s)
 		return ;

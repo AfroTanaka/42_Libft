b0ea036d410e93bcb28db5638f9c41b90a8ad960
Mitsutaka Miura <mmiura@c1r1s2.42tokyo.jp>
2024-07-06 11:58:39 +0900

    Added comments to ft_lstlast.c

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,13 +6,18 @@
 /*   By: mmiura <mmiura@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 11:42:09 by mmiura            #+#    #+#             */
-/*   Updated: 2024/07/06 11:48:10 by mmiura           ###   ########.fr       */
+/*   Updated: 2024/07/06 11:58:12 by mmiura           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 t_list	*ft_lstlast(t_list *lst)
+/**
+ * @brief Returns the last node of the list.
+ * @param (lst) The beginning of the list
+ * @return (t_list*) Last node of the list
+ */
 {
 	t_list	*tmp;
 

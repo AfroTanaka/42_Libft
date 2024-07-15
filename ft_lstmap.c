536aefdac77e4e53f4663fca3212bc8697266b2f
Mitsutaka Miura <mmiura@c1r1s4.42tokyo.jp>
2024-07-15 13:25:49 +0900

    Added comments to ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mmiura <mmiura@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 13:45:01 by mmiura            #+#    #+#             */
-/*   Updated: 2024/07/15 12:52:11 by mmiura           ###   ########.fr       */
+/*   Updated: 2024/07/15 13:25:26 by mmiura           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 #include <stdlib.h>
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+/**
+ * @brief Iterates the list `lst` and applies the function `f` on the content of each node. Creates a new list resulting of the successive applications of the function `f`. The `del` function is used to delete the content of a node if needed.
+ * @param (lst) The address of a pointer to a node.
+ * @param (f) The address of the function used to iterate on the list.
+ * @param (del) The address of the function used to delete the content of a node if needed.
+ * @return (t_list *) Returns the new list. NULL if the allocation fails.
+ */
 {
 	t_list	*start;
 	t_list	*tmp;
